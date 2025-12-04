2e6be55f0ac2198c739d5feef33aeb75ce6f7c2d
Francisco Parasca <francisco.nunes.parasca@gmail.com>
2025-12-04 10:57:44 +0000

    Update SimpleEliminator.java

--- SimpleEliminator.java
+++ SimpleEliminator.java
@@ -1,1 +1,32 @@
+public class SimpleEliminator implements Eliminator {
+
+    @Override
+    public int eliminateSequence(Symbol[] seq, int blockSize, Symbol nothing) {
+
+        int eliminated = 0;
+        int i = 0;
+
+        while (i < seq.length) {
+
+            Symbol current = seq[i];
+            int start = i;
+            int count = 1;
+
+            i++;
+            while (i < seq.length && seq[i].equals(current)) {
+                count++;
+                i++;
+            }
+
+            if (!current.equals(nothing) && count >= blockSize) {
+                for (int k = start; k < start + count; k++) {
+                    seq[k] = nothing;
+                }
+                eliminated += count;
+            }
+        }
+
+        return eliminated;
+    }
+}
 
